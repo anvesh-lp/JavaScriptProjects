6a17feefd36e3934b5cf0aaaf4f30b842eec5f48
Naga Anvesh <knanvesh.01@gmail.com>
2021-06-03 20:01:23 +0530

    Adding local storage functionality also updating deleting and clearing, closes #2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,6 +9,9 @@ loadEventListeners();
 
 
 function loadEventListeners() {
+// DOM load event
+    document.addEventListener("DOMContentLoaded", getTasks);
+
 //    on clicking add task
     form.addEventListener(`submit`, addnewTask);
 //    Delete task
@@ -20,12 +23,40 @@ function loadEventListeners() {
 
 }
 
+//TO get tasks from local storage
+function getTasks() {
+    let tasksArray = [];
+    if (localStorage.getItem('tasks') != null) {
+        tasksArray = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasksArray.forEach(function (x) {
+        createLI(x);
+    });
+
+}
+
+// TO delete from local storage
+function deleteFromLocalStorage(task) {
+    let tasksArray = [];
+    if (localStorage.getItem('tasks') != null) {
+        tasksArray = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasksArray.forEach(function (x, index) {
+        if (x === task.textContent) {
+            tasksArray.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+
+}
+
 // Delete a task
 function deleteTask(event) {
     console.log(event.target);
     if (event.target.parentElement.classList.contains("delete-item")) {
         if (confirm("are you sure?"))
             event.target.parentElement.parentElement.remove();
+        deleteFromLocalStorage(event.target.parentElement.parentElement);
     }
 }
 
@@ -52,6 +83,7 @@ function clearTasks(e) {
     while (tasklist.firstChild) {
         tasklist.removeChild(tasklist.firstChild);
     }
+    localStorage.setItem("tasks", JSON.stringify([]));
 }
 
 
@@ -60,17 +92,36 @@ function addnewTask(event) {
     if (task.value === '') {
         alert(" add a task ");
     } else {
-        // create element
-        const listelement = document.createElement('li');
-        // add class
-        listelement.classList.add('collection-item');
-        //Appending node to child
-        listelement.appendChild(document.createTextNode(task.value));
-        const anchor = document.createElement('a');
-        anchor.className = 'delete-item secondary-content';
-        anchor.innerHTML = '<i class="fas fa-trash-alt"></i>';
-        listelement.appendChild(anchor);
-        tasklist.appendChild(listelement);
+        createLI(task.value);
+        saveToLocalStorage(task.value);
         task.value = '';
     }
+}
+
+//To save task value to local storage
+function saveToLocalStorage(value) {
+    let tasksArray = [];
+    if (localStorage.getItem('tasks') != null) {
+        tasksArray = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasksArray.push(value);
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+}
+
+
+//TO create li element with task value in that list
+
+function createLI(task) {
+    // create element
+    const listelement = document.createElement('li');
+    // add class
+    listelement.classList.add('collection-item');
+    //Appending node to child
+    listelement.appendChild(document.createTextNode(task));
+    const anchor = document.createElement('a');
+    anchor.className = 'delete-item secondary-content';
+    anchor.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    listelement.appendChild(anchor);
+    tasklist.appendChild(listelement);
+    //Save to local storage
 }
